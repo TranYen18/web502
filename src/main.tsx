98c76f47a3cf73ca6160bd2bae1183fc11b439bf
unknown <tranyen18012001@gmail.com>
2022-03-09 09:38:57 +0700

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const person = {
+  name: "Yen",
+  age : 20,
+  school: "fpoly",
+  status: true,
+  children:[
+      {id: "1", name: "Trung"},
+      {id: "2" ,name: "Kien"},
+    ] 
+
+}
+// Sử dụng hàm trong reactjs 
+const showAge = (age) => <p>Tuoi cua ban la {age}</p> ;
+// Sử dụng component trong react
+const ShowAge = props => <p>Tuoi cua ban la {props.age}</p> ;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <p>Tên: {person.name} </p>
+    <p>Tuổi: {person.age} </p>
+    <p>Trường: {person.school} </p>
+    <p>{person.status ? "Da ket hon" : "Chua ket hon"}</p>
+    <ul>
+      {person.children.map(child => <li>
+        {child.name}
+      </li>)}
+    </ul>
+    <div>function: {showAge(person.age)}</div>
+    <div>Component: <ShowAge age={ person.age}/></div>
+  </div>,
   document.getElementById('root')
-)
+);
